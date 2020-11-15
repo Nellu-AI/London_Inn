3f8f44e3c414a376ddf54510e103c3f7cee8a3ae
Ravil S <ravil.nell@gmail.com>
2020-11-15 18:47:50 +0300

    Carousel, Navbar upgrade, add ABOUT SECTION

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function openNavBar() {
+  let icon = document.getElementById("icon-img");
+  let x = document.getElementById("myTopnav");
+  if (!x.classList.contains("responsive")) {
+    x.classList.add("responsive");
+    icon.className = "fas fa-times";
+  } else {
+    x.classList.remove("responsive");
+    icon.className = "fa fa-bars";
+  }
+
+}
